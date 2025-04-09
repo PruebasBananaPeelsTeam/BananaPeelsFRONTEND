a8189d3969e587f2aaa69e38bbb7b77555e080cc
Aida Fuentes <afdlf236@gmail.com>
2025-04-09 21:15:51 +0200

    Update Advert.jsx

--- Wallaclone-Frontend/src/pages/adverts/Advert.jsx
+++ Wallaclone-Frontend/src/pages/adverts/Advert.jsx
@@ -1,22 +1,28 @@
 //Como el modelo de Advert
 
 const Advert = ({ advert }) => {
-  const { name, description, price, photo, type, owner } = advert
+  const { name, description, price, type, image, owner } = advert
+
+  const baseURL = import.meta.env.VITE_API_URL
+
+  const imageUrl = image
+    ? `${baseURL}/images/${image}`
+    : 'http://placehold.co/400x200'; 
 
   return (
     <article className="advert">
       <div className="advert-details">
         <div>
-          <img
-            src={photo || 'http://placehold.co/400x200'}
-            alt=""
-            className="advert-photo"
+        <img
+          src={imageUrl}
+          alt=""
+          className="advert-photo"
           />
         </div>
-        <h2 className="advert-name">{name}</h2>
-        <p className="advert-description">{description}</p>
-        <p className="advert-price">{price} Euros</p>
-        <p className="advert-type">{type ? 'For sale' : 'For wanted'}</p>
+        <h2 className="advert-name">Name: {name}</h2>
+        <p className="advert-description">Description: {description}</p>
+        <p className="advert-price">Price: {price} €</p>
+        <p className="advert-type">Type: {type ? 'For sale' : 'For wanted'}</p>
         <p className="advert-owner">Owner: {owner}</p>
       </div>
     </article>
