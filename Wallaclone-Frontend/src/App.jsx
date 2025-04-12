ba1951680665fef5079da12112e1ed096804e8ee
RoberGF <rgfrasta@gmail.com>
2025-04-12 17:05:01 +0200

    WIP: 404 page added

--- Wallaclone-Frontend/src/App.jsx
+++ Wallaclone-Frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route, Outlet } from 'react-router-dom'
 import LoginPage from './pages/auth/loginPage'
 import RegisterPage from './pages/auth/registerPage'
 import Layout from './components/layout/layout'
+import NotFoundPage from './components/shared/notFoundPage'
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
         }
       ></Route>
       <Route path="/register" element={<RegisterPage />} />
+      <Route path="*" element={<NotFoundPage />} />
     </Routes>
   )
 }
