4e7ae81e744cc59903b466cd9cdbadc26836b57e
Javier Rodriguez <franciscoromera0816@gmail.com>
2025-04-22 21:04:20 +0200

    fix: -Corregido error para crear anuncio con tipo "wanted"

--- Wallaclone-Frontend/src/pages/adverts/createAdvertPage.jsx
+++ Wallaclone-Frontend/src/pages/adverts/createAdvertPage.jsx
@@ -167,7 +167,7 @@ const CreateAdvertPage = () => {
                 className="flex w-full border border-gray-300 rounded-xl px-4 py-2 focus:outline-none focus:ring-2 focus:ring-[rgb(223,184,13)]"
               >
                 <option value="sell">On sale</option>
-                <option value="wanted">Wanted</option>
+                <option value="buy">Wanted</option>
               </select>
             </label>
           </div>
