51aea98daad121ebec2a66caf0e4fb6d4e5c8947
Aida Fuentes <afdlf236@gmail.com>
2025-04-23 12:53:35 +0200

    wip: modify header styling

--- Wallaclone-Frontend/src/components/layout/header.jsx
+++ Wallaclone-Frontend/src/components/layout/header.jsx
@@ -1,20 +1,22 @@
-import { Link } from 'react-router-dom'
-import Burger from '../shared/burguer.jsx'
-import Logout from '../shared/logout.jsx'
-import { useAuth } from '../../context/AuthContext.jsx'
+import { Link } from 'react-router-dom';
+import Burger from '../shared/burguer.jsx';
+import Logout from '../shared/logout.jsx';
+import { useAuth } from '../../context/AuthContext.jsx';
 
 export default function Header() {
-  const { isAuthenticated } = useAuth()
+  const { isAuthenticated } = useAuth();
 
   return (
-    <header className="bg-gradient-to-l from-yellow-600 via-orange-500 to-red-800 py-1 px-4">
+    <header
+      className="py-2 px-4 bg-cover bg-center bg-no-repeat"
+      style={{ backgroundImage: "url('/images/header3.jpg')" }}
+    >
       <div className="relative max-w-7xl mx-auto flex justify-between items-center">
         <div className="text-[20px] font-extrabold bg-gradient-to-r from-yellow-400 via-green-400 to-orange-400 text-transparent bg-clip-text drop-shadow-2xl">
           <p>BananaPeels</p>
         </div>
         <Burger />
         {isAuthenticated && <Logout />}
-        
       </div>
     </header>
   );
