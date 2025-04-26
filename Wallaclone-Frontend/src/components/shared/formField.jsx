06a0708c764bb06e44d1b29a790862690bdc47ac
Javier Rodriguez <franciscoromera0816@gmail.com>
2025-04-26 12:21:07 +0200

    fix:
    me traigo formField de develop y se adapata a lao que pedimos

--- Wallaclone-Frontend/src/components/shared/formField.jsx
+++ Wallaclone-Frontend/src/components/shared/formField.jsx
@@ -3,19 +3,19 @@ import React from 'react'
 const FormField = React.memo(function FormField({ label, error, ...props }) {
   return (
     <div className="flex flex-col w-full">
-      <label htmlFor={props.name} className="text-sm font-medium text-gray-700 mb-2">
-        {label}
+      <label className="text-sm font-medium text-gray-700 mb-2">
+        <span>{label}</span>
+        <input
+          className={`flex w-full border rounded-xl px-4 py-2 focus:outline-none focus:ring-2 ${
+            error
+              ? 'border-red-500 focus:ring-red-300'
+              : 'border-gray-300 focus:ring-[rgb(223,184,13)]'
+          }`}
+          autoComplete="on"
+          {...props}
+        />
       </label>
-      <input
-        id={props.name}
-        className={`flex w-full border rounded-xl px-4 py-2 focus:outline-none focus:ring-2 ${
-          error
-            ? 'border-red-500 focus:ring-red-300'
-            : 'border-gray-300 focus:ring-[rgb(223,184,13)]'
-        }`}
-        autoComplete="on"
-        {...props}
-      />
+
       {error && <span className="text-sm text-red-500 mt-1">{error}</span>}
     </div>
   )
