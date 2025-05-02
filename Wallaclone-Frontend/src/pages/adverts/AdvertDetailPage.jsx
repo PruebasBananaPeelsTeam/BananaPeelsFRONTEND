c0b323c04967b2ba5924339145214017324d0413
Javier Rodriguez <franciscoromera0816@gmail.com>
2025-05-02 13:00:00 +0200

    wip: translation
    AdvertDetailPage

--- Wallaclone-Frontend/src/pages/adverts/AdvertDetailPage.jsx
+++ Wallaclone-Frontend/src/pages/adverts/AdvertDetailPage.jsx
@@ -1,9 +1,11 @@
+// AdvertDetailPage.jsx
 import { useNavigate, useParams, Link } from 'react-router-dom'
 import { useEffect, useState } from 'react'
 import {
   getAdvertDetail,
   addFavorite,
   removeFavorite,
+  toggleSoldAdvert,
 } from '../../services/adverts-service'
 import { isApiClientError } from '../../api/client'
 import Page from '../../components/layout/page'
@@ -14,14 +16,12 @@ import AdvertStatus from '../../components/shared/advertStatus'
 import Button from '../../components/shared/button'
 import { useTranslation } from 'react-i18next'
 import { checkChatByAdvert, getOrCreateChat } from '../../services/chat-service'
-import { toggleSoldAdvert } from '../../services/adverts-service'
 import { FaCheckCircle } from 'react-icons/fa'
 import DeleteAdvertPage from './DeleteAdvertPage'
 import { Heart, HeartOff } from 'lucide-react'
 
-
 function AdvertDetailPage() {
-  const { t } = useTranslation() //  Hook de traducción
+  const { t } = useTranslation()
   const params = useParams()
   const navigate = useNavigate()
   const [advert, setAdvert] = useState(null)
@@ -29,22 +29,17 @@ function AdvertDetailPage() {
   const { user, updateUserData } = useAuth()
   const [isFavorite, setIsFavorite] = useState(false)
 
-  // Obtener anuncio
   useEffect(() => {
     if (params.advertId && params.slug) {
       setLoading(true)
-
       getAdvertDetail(params.advertId, params.slug)
         .then((advert) => {
-          advert._id = advert._id.toString() // 👈 aseguramos el formato
+          advert._id = advert._id.toString()
           setAdvert(advert)
-
-          // Comprobamos si está en favoritos
           const fav = user?.favorites?.some(
             (favId) => favId.toString() === advert._id,
           )
           setIsFavorite(fav)
-
           setLoading(false)
         })
         .catch((error) => {
@@ -58,27 +53,19 @@ function AdvertDetailPage() {
     }
   }, [params.advertId, params.slug, navigate, user])
 
-  // inicializador de chats
   const handleStartChat = async () => {
     try {
-      const existingChat = await checkChatByAdvert(advert._id) // O advert.id si no es _id
-
-      if (existingChat) {
-        navigate(`/chat/room/${existingChat._id}`)
-      } else {
-        const newChat = await getOrCreateChat(advert._id)
-        navigate(`/chat/room/${newChat._id}`)
-      }
+      const existingChat = await checkChatByAdvert(advert._id)
+      const chat = existingChat || (await getOrCreateChat(advert._id))
+      navigate(`/chat/room/${chat._id}`)
     } catch (error) {
       console.error('Error al iniciar o encontrar chat:', error)
     }
   }
 
-  // Añadir o quitar favorito
   const handleFavoriteToggle = async () => {
     try {
       let updatedFavorites
-
       if (isFavorite) {
         await removeFavorite(advert._id)
         updatedFavorites = user.favorites.filter(
@@ -88,14 +75,8 @@ function AdvertDetailPage() {
         await addFavorite(advert._id)
         updatedFavorites = [...(user.favorites || []), advert._id.toString()]
       }
-
       updateUserData({ ...user, favorites: updatedFavorites })
       setIsFavorite(!isFavorite)
-
-      // Depuración
-      console.log('✅ Favorito actualizado:', !isFavorite)
-      console.log('🧠 updatedFavorites:', updatedFavorites)
-      console.log('🆔 advert._id:', advert._id.toString())
     } catch (error) {
       console.error('❌ Error actualizando favorito:', error)
     }
@@ -119,32 +100,27 @@ function AdvertDetailPage() {
           <div className="text-black">
             <img
               src={imageUrl}
-              alt={advert?.name || t('advertDetail.noImage')} // 👈 Traducción
+              alt={advert?.name || t('advertDetail.noImage')}
               className="w-full max-h-[300px] object-scale-down rounded-xl mb-2 mx-auto"
             />
-
             <div className="max-w-2xl mx-auto text-justify space-y-2 bg-gray-100 p-4 rounded-xl shadow">
               <p>
                 <strong>{t('advertDetail.description')}:</strong>{' '}
                 {advert.description}
               </p>
-
               <p>
                 <strong>{t('advertDetail.price')}:</strong> {advert.price} €
               </p>
-
               <p>
                 <strong>{t('advertDetail.type')}:</strong>{' '}
                 {advert.type === 'buy'
                   ? t('advertDetail.typeWanted')
                   : t('advertDetail.typeForSale')}
               </p>
-
               <p>
                 <strong>{t('advertDetail.categories')}:</strong>{' '}
                 {advert.tags.join(', ')}
               </p>
-
               <p>
                 <strong>{t('advertDetail.seller')}:</strong>{' '}
                 <Link
@@ -156,28 +132,21 @@ function AdvertDetailPage() {
               </p>
             </div>
 
-            {/* Botones y estado */}
-
             <div className="flex flex-col md:flex-row flex-wrap gap-2 mt-6">
-              {/* Botón Back */}
               <div>
-                <Button
-                  onClick={() => navigate('/')}
-                  className="w-full md:w-auto"
-                >
-                  ← Back
+                <Button onClick={() => navigate('/')} className="w-full md:w-auto">
+                  {t('advertDetail.backButton')}
                 </Button>
               </div>
 
-              {/* Condición para mostrar los botones de actualización, eliminación y reserva */}
-              {/* chat button */}
               {user && advert.owner._id !== user._id && (
                 <div className="flex justify-center my-4">
-                  <Button onClick={handleStartChat}>💬 Chat</Button>
+                  <Button onClick={handleStartChat}>
+                    {t('advertDetail.chatButton')}
+                  </Button>
                 </div>
               )}
 
-              {/* Update button */}
               {user && advert.owner._id === user._id && advert._id && (
                 <div className="flex flex-col md:flex-row gap-2 w-full md:w-auto">
                   <div>
@@ -204,8 +173,11 @@ function AdvertDetailPage() {
                     }}
                     className="mb-4 ml-4"
                   >
-                    {advert?.sold ? '✔ Mark as Unsold' : '💰 Mark as Sold'}
+                    {advert?.sold
+                      ? t('advertDetail.markAsUnsold')
+                      : t('advertDetail.markAsSold')}
                   </Button>
+
                   <ReservedToggleButton
                     advert={advert}
                     className="w-full md:w-auto"
@@ -220,11 +192,11 @@ function AdvertDetailPage() {
                 <Button onClick={handleFavoriteToggle} className="mb-4 ml-2">
                   {isFavorite ? (
                     <span className="flex items-center gap-2 text-red-500">
-                      <HeartOff size={18} /> Quitar de favoritos
+                      <HeartOff size={18} /> {t('advertDetail.removeFromFavorites')}
                     </span>
                   ) : (
                     <span className="flex items-center gap-2 text-green-600">
-                      <Heart size={18} /> Añadir a favoritos
+                      <Heart size={18} /> {t('advertDetail.addToFavorites')}
                     </span>
                   )}
                 </Button>
@@ -237,10 +209,11 @@ function AdvertDetailPage() {
                   textSize="text-xl"
                 />
               </div>
+
               {advert.sold && (
                 <div className="flex items-center gap-2 bg-red-700 text-white px-3 py-1 rounded text-sm font-semibold shadow ml-4">
                   <FaCheckCircle size={16} />
-                  Sold
+                  {t('advertDetail.sold')}
                 </div>
               )}
             </div>
